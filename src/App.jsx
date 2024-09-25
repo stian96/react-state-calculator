5be6e9cea3994e4fa74e7f95c4605c476e780c38
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-25 11:38:57 +0200

    finished core and extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,94 @@
+
+import React, { useState } from "react";
 import "./App.css"
 
-function App() {
+const App = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [operator, setOperator] = useState('+');
+  const [result, setResult] = useState(0);
+
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const operators = ['+', '-', '*', '÷'];
+
+  const clearNum1 = () => setNum1('');
+  const clearNum2 = () => setNum2('');
+
+  const calculateAnswer = () => {
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    switch (operator) {
+      case '+': setResult(number1 + number2);
+        break;
+      case '-': setResult(number1 - number2);
+        break;
+      case '*': setResult(number1 * number2);
+        break;
+      case '÷': setResult(number1 / number2);
+        break;
+    }
+  }
+
+  const handleNumberClick = (number, isNum1=true) => {
+    if (isNum1) {
+      if (num1 === '0' && number === '0') 
+        return;
+      if (num1 === '0') {
+        setNum1(number);
+      }
+      else {
+        setNum1((prev) => prev + number.toString());
+      }
+    }
+    else {
+      if (num2 === '0' && number === '0') 
+        return;
+      if (num2 === '0') {
+        setNum2(number);
+      }
+      else {
+        setNum2((prev) => prev + number.toString())
+      }
+    }
+  }
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{num1 === '' ? 0 : num1}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          { numbers.map((number) => (
+            <button key={number} onClick={() => handleNumberClick(number, true)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={clearNum1}>Clear</button>
         </div>
       </div>
-
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          { operators.map((operator, index) => (
+            <button key={index} onClick={() => setOperator(operator)}>{operator}</button>
+          ))}
         </div>
       </div>
-
       <div className="panel">
-        <p>0</p>
+        <p>{num2 === '' ? 0 : num2}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+        { numbers.map((number) => (
+            <button key={number} onClick={() => handleNumberClick(number, false)}>
+              {number}
+            </button>
+          ))}
+          <button onClick={clearNum2}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{isNaN(result) ? 0 : result}</p>
         <div>
-          <button>=</button>
+          <button onClick={calculateAnswer}>=</button>
         </div>
       </div>
     </div>
